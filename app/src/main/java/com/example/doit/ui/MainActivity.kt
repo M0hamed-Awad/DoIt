fe6c44d0abef78c7c7fc92e5b67527a6376766e9
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-03-02 19:12:43 +0200

    Add custom splash screen

--- app/src/main/java/com/example/doit/ui/MainActivity.kt
+++ app/src/main/java/com/example/doit/ui/MainActivity.kt
@@ -1,14 +1,16 @@
-package com.example.doit
+package com.example.doit.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.doit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
