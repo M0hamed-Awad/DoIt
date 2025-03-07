851f68370b216cdcee783f887c62ca81669f8aed
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-03-07 22:54:28 +0200

    Design Add Task Screen

--- app/src/main/java/com/example/doit/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/doit/ui/AddTaskActivity.kt
@@ -1,34 +1,21 @@
 package com.example.doit.ui
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import com.example.doit.R
-import com.example.doit.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class AddTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_add_task)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.addFab.setOnClickListener {
-            val intent = Intent(this, AddTaskActivity::class.java)
-            startActivity(intent)
         }
     }
 }
